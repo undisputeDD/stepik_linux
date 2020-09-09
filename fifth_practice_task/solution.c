3b4e2c227f829c67679d0c41776743877e974f82
akreidun <kreyduna@gmail.com>
2020-09-09 23:30:06 +0300

    creating daemon processing signal

--- fifth_practice_task/solution.c
+++ fifth_practice_task/solution.c
@@ -1,17 +1,27 @@
+#include <sys/types.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
+
+void mysignal_handler(int signal)
+{
+        printf("%d\n", getpid());
+        exit(0);
+}
 
 int main()
 {
-	pid_t pID = fork();
-	if (pID == 0)
-	{
-		chdir("/");
-		setsid();
-		close(0);
-		printf("%d\n", getpid());
-		close(1);
-		close(2);
-	}
-	return 0;
+    pid_t pID = fork();
+    if (pID == 0)
+    {
+        chdir("/");
+        setsid();
+        close(0);
+        close(1);
+        close(2);
+        signal(SIGURG, mysignal_handler);
+        sleep(1000);
+    }
+    return 0;
 }
